344d52a02c7b05082358b8917332079d2f779bb7
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-01 23:51:13 -0500

    Make color output random

--- script.js
+++ script.js
@@ -8,12 +8,42 @@ function createEtch(gridSize){
       let square2 = document.createElement("div");
       square2.classList.add("row");
       square2.addEventListener("mouseover", () => {
-        square2.style.backgroundColor = "black";
+        let randomColor2 = randomColor();
+        square2.style.backgroundColor = randomColor2;
       })
       square.appendChild(square2);
     }
     container.appendChild(square);
   }
+}
+
+function randomColor() {
+  let color = ''
+  let randoNumber = Math.floor(Math.random()*5)
+  switch(randoNumber){
+    case 0:
+      color = "rgb(0, 255, 145)"
+      break;
+    case 1: 
+      color = "orange";
+      break;
+    case 2: 
+      color = "yellow";
+      break;
+    case 3: 
+      color = "green";
+      break;
+    case 4: 
+      color = "blue";
+      break;
+    case 5: 
+      color = "red";
+      break;
+    default: 
+      color = "black";
+      break; 
+  }
+  return color;
 }
 
 let button = document.querySelector("button");
