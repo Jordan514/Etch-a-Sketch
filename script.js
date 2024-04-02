bc01d2a95ff88441ce6fa2257a7b124b09d1bf34
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2024-04-01 23:08:01 -0500

    Something I forget

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-let gridSize = 100;
 let container = document.querySelector("#container");
-for(let i = 0;i<gridSize;i++){
-  let square = document.createElement("div");
-  square.classList.add("coloum");
+
+function createEtch(gridSize){
   for(let i = 0;i<gridSize;i++){
-    let square2 = document.createElement("div");
-    square2.classList.add("row");
-    square2.addEventListener("mouseover", () => {
-      square2.style.backgroundColor = "black";
-    })
-    square.appendChild(square2);
+    let square = document.createElement("div");
+    square.classList.add("coloum");
+    for(let i = 0;i<gridSize;i++){
+      let square2 = document.createElement("div");
+      square2.classList.add("row");
+      square2.addEventListener("mouseover", () => {
+        square2.style.backgroundColor = "black";
+      })
+      square.appendChild(square2);
+    }
+    container.appendChild(square);
   }
-  container.appendChild(square);
 }
+
+createEtch(16);
+
+let button = document.querySelector("button");
+function clearGrid (){
+  container.remove();
+}
+button.addEventListener("click", ()=> {
+  clearGrid();
+  
+})
